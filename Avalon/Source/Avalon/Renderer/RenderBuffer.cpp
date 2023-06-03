464c702d0261791c3ab5fd9e2d256e9115f5b3b2
Alesinx <Alejandro7mr@gmail.com>
2023-06-03 16:41:54 +0200

    Missing file to previous commit

--- Avalon/Source/Avalon/Renderer/RenderBuffer.cpp
+++ Avalon/Source/Avalon/Renderer/RenderBuffer.cpp
@@ -21,7 +21,7 @@ namespace Avalon
 		return nullptr;
 	}
 
-	IndexBuffer* IndexBuffer::Create(const uint32_t* const indices, const uint32_t size)
+	IndexBuffer* IndexBuffer::Create(const uint32_t* const indices, const uint32_t count)
 	{
 		switch (Renderer::GetAPI())
 		{
@@ -29,10 +29,54 @@ namespace Avalon
 			AVALON_CORE_ASSERT(false, "RendererAPI::None is currently not supported")
 				return nullptr;
 		case RendererAPI::OpenGL:
-			return new OpenGLIndexBuffer(indices, size);
+			return new OpenGLIndexBuffer(indices, count);
 		}
 
 		AVALON_CORE_ASSERT(false, "Unknown RendererAPI");
 		return nullptr;
+	}
+
+	void BufferLayout::CalculateOffsetAndStride()
+	{
+		uint32_t offset = 0;
+		mStride = 0;
+		for (BufferElement& element : mElements)
+		{
+			element.offset = offset;
+			offset += element.size;
+			mStride += element.size;
+		}
+	}
+
+	uint32_t BufferElement::GetComponentCount() const
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:
+			return 4;
+		case ShaderDataType::Float2:
+			return 2;
+		case ShaderDataType::Float3:
+			return 3;
+		case ShaderDataType::Float4:
+			return 4;
+		case ShaderDataType::Mat3:
+			return 3 * 3;
+		case ShaderDataType::Mat4:
+			return 4 * 4;
+		case ShaderDataType::Int:
+			return 1;
+		case ShaderDataType::Int2:
+			return 2;
+		case ShaderDataType::Int3:
+			return 3;
+		case ShaderDataType::Int4:
+			return 4;
+		case ShaderDataType::Bool:
+			return 1;
+		}
+
+		AVALON_CORE_ASSERT(false, "Unknown ShaderDataType");
+		return 0;
 	}
 }
